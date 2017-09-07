c6f3b460c8f32eb81782688947c776ed1c4484a1
Ambroise <amb.soullier@gmail.com>
2017-09-07 11:48:14 +0200

    created Models for member and promotion

--- CodeReviewMeeting/src/fr/epf/models/Promotion.java
+++ CodeReviewMeeting/src/fr/epf/models/Promotion.java
@@ -1,5 +1,23 @@
 package fr.epf.models;
 
 public class Promotion {
+	
+	private String name;
+	private int number;
+	
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getNumber() {
+		return number;
+	}
+	public void setNumber(int number) {
+		this.number = number;
+	}
+	
 
 }
