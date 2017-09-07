27d0ba0f321197f3214422872ac21a23cdbf3f3c
IPichon <iris.pichon@free.fr>
2017-09-07 13:55:04 +0200

    Filled memberDAO class with method needed.

--- CodeReviewMeeting/src/fr/epf/dao/MemberDAO.java
+++ CodeReviewMeeting/src/fr/epf/dao/MemberDAO.java
@@ -1,5 +1,26 @@
 package fr.epf.dao;
 
+import java.util.List;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import fr.epf.models.Member;
+
+
 public class MemberDAO {
+	@PersistenceContext
+	private EntityManager entityManager;
+	
+	public void save(Member member) {
+		entityManager.persist(member);
+	}
+	
+	public Member findOne(Long id) {
+		return entityManager.find(Member.class, id);
+	}
+	
+	public List<Member> findAll() {
+		return entityManager.createQuery("SELECT * FROM member").getResultList();
+	}
+
 
 }
